64e7269fd13fd93950218941efc229384d8894c8
Justin Malčić <j.malcic@me.com>
2025-12-14 16:51:30 +0000

    Add helpers

--- lib/rcharts.rb
+++ lib/rcharts.rb
@@ -6,6 +6,17 @@ require 'rcharts/engine'
 module RCharts
   extend ActiveSupport::Autoload
 
+  mattr_accessor :series_color_classes, default: %w[blue red green orange purple]
+  mattr_accessor :series_symbols, default: %w[● ■ ◆ ▲ ▼]
+
   autoload :Percentage
   autoload :Type
+
+  def self.color_class_for(index)
+    series_color_classes[index % series_color_classes.size]
+  end
+
+  def self.symbol_for(index)
+    series_symbols[index % series_symbols.size]
+  end
 end
--- test/rcharts_test.rb
+++ test/rcharts_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class RChartsTest < ActiveSupport::TestCase
-  test 'it has a version number' do
-    assert RCharts::VERSION
+  test 'has version number' do
+    assert_not_empty RCharts::VERSION
+  end
+
+  test 'returns color class for index' do
+    assert_not_empty RCharts.color_class_for(0)
+    assert_not_empty RCharts.color_class_for(RCharts.series_color_classes.size)
+  end
+
+  test 'returns symbol for index' do
+    assert_not_empty RCharts.symbol_for(0)
+    assert_not_empty RCharts.symbol_for(RCharts.series_symbols.size)
   end
 end

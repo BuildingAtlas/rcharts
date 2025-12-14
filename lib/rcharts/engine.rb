8cb4408168757dc1f673b9746862f8fa53677637
Justin Malčić <j.malcic@me.com>
2025-12-14 16:56:53 +0000

    Add initializers

--- lib/rcharts/engine.rb
+++ lib/rcharts/engine.rb
@@ -7,5 +7,19 @@ module RCharts
     ActiveSupport::Inflector.inflections(:en) do |inflect|
       inflect.acronym 'RCharts'
     end
+
+    config.rcharts = ActiveSupport::OrderedOptions.new
+    config.rcharts.series_color_classes = %w[blue red green orange purple]
+    config.rcharts.series_symbols = %w[● ■ ◆ ▲ ▼]
+
+    initializer 'rcharts.configure' do |app|
+      RCharts.series_color_classes = app.config.rcharts.series_color_classes
+      RCharts.series_symbols = app.config.rcharts.series_symbols
+    end
+
+    initializer 'rcharts.types' do
+      ActiveModel::Type.register :'rcharts/percentage', RCharts::Type::Percentage
+      ActiveModel::Type.register :'rcharts/symbol', RCharts::Type::Symbol
+    end
   end
 end

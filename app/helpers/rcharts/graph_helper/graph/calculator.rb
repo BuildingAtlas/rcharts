f3476e88b2ca927416379ba8def6cb5d52a3c33b
Justin Malčić <j.malcic@me.com>
2025-12-15 17:47:33 +0000

    Add calculator

---
+++ app/helpers/rcharts/graph_helper/graph/calculator.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+module RCharts
+  module GraphHelper
+    module Graph
+      class Calculator
+        delegate :[], :dig, to: :to_h
+
+        def initialize(values = {})
+          @values = values
+          @chain = []
+          @selections = {}
+        end
+
+        def sum_complete
+          values.transform_values do |value|
+            case value
+            when Hash then value.values.any?(nil) ? nil : value.values.sum
+            when Array then value.any?(nil) ? nil : value.sum
+            else value
+            end
+          end
+        end
+
+        def signed(sign = nil)
+          return self unless sign
+
+          apply sign do
+            values_for_predicate sign == :positive ? :positive? : :negative?
+          end
+        end
+
+        def stacked(exclude_current: false)
+          apply :stacked, exclude_current do
+            collect_sum inclusive: !exclude_current
+          end
+        end
+
+        def to_h
+          keys.index_with do |key|
+            next values if key.nil?
+
+            values.transform_values { it[key] }
+          end
+        end
+
+        protected
+
+        attr_accessor :values, :chain
+
+        def apply!(*operation)
+          selections[[*chain, *operation]] ||= yield if block_given?
+          self.chain = chain.dup
+          chain.append(*operation)
+          self.values = selections[chain]
+        end
+
+        private
+
+        attr_reader :selections
+
+        def apply(*operation, &)
+          selections[[*chain, *operation]] ||= yield if block_given?
+          dup.tap { it.apply!(*operation) }
+        end
+
+        def keys
+          case values.values.first
+          in Hash => value then value.keys
+          in Array => value then (0...value.size).to_a
+          else [nil]
+          end
+        end
+
+        def values_for_predicate(predicate)
+          values.deep_transform_values { it.try(predicate) == false ? nil : it }
+        end
+
+        def collect_sum(inclusive: true)
+          values.transform_values do |value|
+            value.keys.index_with do |key|
+              value.keys.index(key).then do |limit|
+                value.values.slice(inclusive ? 0..limit : 0...limit).compact.presence&.sum
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/helpers/rcharts/graph_helper/graph/calculator_test.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module RCharts
+  module GraphHelper
+    module Graph
+      class CalculatorTest < ActiveSupport::TestCase
+        VALUES = { 1 => { first: 1, second: 2, third: 1 }, 2 => { first: 20.0, second: -30.0, third: 40 },
+                   3 => { first: -14.0, second: -20.0, third: nil } }.freeze
+
+        setup do
+          @calculator = Calculator.new(VALUES)
+          @empty_calculation = Calculator.new
+        end
+
+        test 'selects positive and negative values' do
+          assert_equal VALUES.keys, @calculator.to_h.values.first.keys
+          assert_equal VALUES.values.pluck(:first), @calculator[:first].values
+        end
+
+        test 'selects positive values' do
+          @calculator.signed(:positive)[:first]
+                     .values
+                     .difference(VALUES.values.pluck(:first).select(&:positive?))
+                     .then do |difference|
+            assert_not_empty difference
+            assert difference.all?(&:nil?)
+          end
+        end
+
+        test 'selects negative values' do
+          @calculator.signed(:negative)[:first]
+                     .values
+                     .difference(VALUES.values.pluck(:first).select(&:negative?))
+                     .then do |difference|
+            assert_not_empty difference
+            assert difference.all?(&:nil?)
+          end
+        end
+
+        test 'selects stacked values' do
+          assert_equal VALUES.values.pluck(:first), @calculator.stacked[:first].values
+          assert_not_equal VALUES.values.pluck(:second), @calculator.stacked[:second]
+          assert_equal VALUES.values.pluck(:first)
+                             .zip(VALUES.values.pluck(:second))
+                             .collect { |first, second| first + second },
+                       @calculator.stacked[:second].values
+        end
+
+        test 'selects previous values' do
+          assert_empty @calculator.stacked(exclude_current: true)[:first].values.compact
+          assert_equal VALUES.values.pluck(:first),
+                       @calculator.stacked(exclude_current: true)[:second].values
+        end
+
+        test 'sums complete values' do
+          assert_equal VALUES[1].values.sum, @calculator.sum_complete[1]
+          assert_equal VALUES[2].values.sum, @calculator.sum_complete[2]
+          assert_nil @calculator.sum_complete[3]
+        end
+      end
+    end
+  end
+end

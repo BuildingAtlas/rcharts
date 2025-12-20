58c89a0d5e2894000ceda4663f75f11e89327e88
Justin Malčić <j.malcic@me.com>
2025-12-20 17:58:34 +0000

    Update demo

--- test/dummy/app/models/sample.rb
+++ test/dummy/app/models/sample.rb
@@ -13,102 +13,47 @@ class Sample
     Pulse.enable! if pulses
   end
 
+  def daily_timeseries
+    (0..30).to_a.to_h { [it.days.from_now.to_date, randomize(100)] }
+  end
+
+  def monthly_timeseries
+    (0..12).to_a.to_h { [it.months.from_now.to_date, randomize(1000)] }
+  end
+
+  def yearly_timeseries
+    (0..5).to_a.to_h { [it.years.from_now.to_date, randomize(10_000)] }
+  end
+
+  def comparison_timeseries
+    (0..10).to_a.to_h { [it.days.from_now.to_date, { current: randomize(100), previous: randomize(80) }] }
+  end
+
+  def sparse_timeseries
+    (0..20).to_a.to_h { [it.days.from_now.to_date, (it % 5).zero? ? nil : randomize(50)] }
+  end
+
+  def categorical_data
+    { 'Apples' => randomize(50), 'Oranges' => randomize(40), 'Bananas' => randomize(60), 'Grapes' => randomize(30) }
+  end
+
+  def multi_series_categorical
+    { 'Q1' => { sales: randomize(100), profit: randomize(20) },
+      'Q2' => { sales: randomize(120), profit: randomize(25) },
+      'Q3' => { sales: randomize(110), profit: randomize(15) },
+      'Q4' => { sales: randomize(150), profit: randomize(40) } }
+  end
+
+  def mixed_sign_data
+    { 'A' => randomize(50), 'B' => randomize(-30), 'C' => randomize(20), 'D' => randomize(-10) }
+  end
+
   def points
-    { 1.0 => randomize(10), 2.0 => randomize(20.5), 3.0 => nil, 4.0 => randomize(4), 5.0 => randomize(40) }
+    (0..10).to_a.to_h { [it.to_f, randomize(100)] }
   end
 
-  def times
-    (0..50).to_a.to_h { [(it * 10).minutes.from_now, (it % 28).zero? ? nil : randomize(100)] }
-  end
-
-  def ugly_points
-    { 1.0 => randomize(190), 2.0 => randomize(103.5), 3.0 => nil, 4.0 => randomize(10) }
-  end
-
-  def negative_points
-    { 1.0 => randomize(-10), 2.0 => randomize(-103.5), 3.0 => randomize(-43), 4.0 => randomize(-1) }
-  end
-
-  def mixed_points
-    { 1.0 => randomize(10), 2.0 => randomize(103.5), 3.0 => randomize(-43), 4.0 => randomize(1) }
-  end
-
-  def series_points
-    { 1.0 => { prediction: randomize(10), actual: randomize(20) },
-      2.0 => { prediction: 0, actual: randomize(20.5) },
-      3.0 => { prediction: randomize(40), actual: randomize(10) },
-      4.0 => { prediction: randomize(50), actual: randomize(50) } }
-  end
-
-  def ugly_series_points
-    { 1.0 => { prediction: randomize(190), actual: randomize(20) },
-      2.0 => { prediction: randomize(103.5), actual: randomize(30) },
-      3.0 => { prediction: randomize(403), actual: randomize(200) },
-      4.0 => { prediction: randomize(10), actual: randomize(500) } }
-  end
-
-  def negative_series_points
-    { 1.0 => { prediction: randomize(-190), actual: randomize(-20) },
-      2.0 => { prediction: randomize(-103.5), actual: randomize(-30) },
-      3.0 => { prediction: randomize(-403), actual: randomize(-200) },
-      4.0 => { prediction: randomize(-10), actual: randomize(-500) } }
-  end
-
-  def mixed_series_points
-    { 1.0 => { prediction: randomize(190), actual: randomize(-20), other: randomize(50) },
-      2.0 => { prediction: randomize(-103.5), actual: randomize(30), other: randomize(-200) },
-      3.0 => { prediction: nil, actual: randomize(-200), other: randomize(-500) },
-      4.0 => { prediction: randomize(403), actual: randomize(200), other: randomize(100) },
-      5.0 => { prediction: randomize(400), actual: randomize(100), other: randomize(50) },
-      7.0 => { prediction: randomize(-10), actual: randomize(-500), other: randomize(-40) } }
-  end
-
-  def categories
-    { 'apples' => randomize(2), 'oranges' => nil, 'bananas' => randomize(1), 'grapes' => randomize(4),
-      'pears' => randomize(5) }
-  end
-
-  def ugly_categories
-    { 'apples' => randomize(200), 'oranges' => randomize(3), 'bananas' => randomize(103.3), 'grapes' => randomize(400),
-      'pears' => randomize(50) }
-  end
-
-  def negative_categories
-    { 'apples' => randomize(-2), 'oranges' => randomize(-3), 'bananas' => randomize(-103.3), 'grapes' => randomize(-4),
-      'pears' => randomize(-5) }
-  end
-
-  def mixed_categories
-    { 'apples' => randomize(20), 'oranges' => randomize(30), 'bananas' => randomize(-103.3), 'grapes' => nil,
-      'pears' => randomize(50) }
-  end
-
-  def series_categories
-    { 'apples' => { prediction: randomize(10), actual: randomize(20), ideal: randomize(30.5) },
-      'oranges' => { prediction: 0, actual: randomize(20.5), ideal: randomize(10.5) },
-      'bananas' => { prediction: randomize(40), actual: nil, ideal: randomize(2.5) },
-      'grapes' => { prediction: randomize(50), actual: randomize(50), ideal: randomize(3.5) } }
-  end
-
-  def ugly_series_categories
-    { 'apples' => { prediction: randomize(190), actual: randomize(20) },
-      'oranges' => { prediction: randomize(103.5), actual: randomize(30) },
-      'bananas' => { prediction: randomize(403), actual: randomize(200) },
-      'grapes' => { prediction: randomize(10), actual: randomize(500) } }
-  end
-
-  def negative_series_categories
-    { 'apples' => { prediction: randomize(-190), actual: randomize(-20), ideal: randomize(-30.5) },
-      'oranges' => { prediction: randomize(-103.5), actual: nil, ideal: randomize(-10) },
-      'bananas' => { prediction: randomize(-403), actual: randomize(-200), ideal: randomize(-20) },
-      'grapes' => { prediction: randomize(-10), actual: randomize(-100), ideal: randomize(-20) } }
-  end
-
-  def mixed_series_categories
-    { 'apples' => { prediction: randomize(190.0), actual: randomize(-20.0), ideal: randomize(30.5) },
-      'oranges' => { prediction: randomize(-103.5), actual: randomize(30), ideal: randomize(10) },
-      'bananas' => { prediction: nil, actual: randomize(100), ideal: randomize(-20) },
-      'grapes' => { prediction: randomize(-10), actual: randomize(-100), ideal: randomize(20) } }
+  def distribution_data
+    (0..20).to_a.to_h { [it, randomize(it * 5)] }
   end
 
   private
